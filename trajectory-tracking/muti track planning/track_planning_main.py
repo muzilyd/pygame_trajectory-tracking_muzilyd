666a9af44c1dbaa1357345bb51e18e08ddc3a522
YunDi Li <73511912+muzilyd@users.noreply.github.com>
2022-01-03 11:03:41 +0800

    Update track_planning_main.py

--- trajectory-tracking/muti track planning/track_planning_main.py
+++ trajectory-tracking/muti track planning/track_planning_main.py
@@ -13,6 +13,8 @@ class TrackGame(object):
         self.clock = pygame.time.Clock()
         # 3. 调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
+        self.LINES_LIST_NEW = []
+        self.path_point = []
 
     def __create_sprites(self):
         bg = Background()
@@ -24,7 +26,8 @@ class TrackGame(object):
     
     def start_game(self):
         print("游戏开始...")
-
+        pygame.gfxdraw.bezier(self.screen, [(100,620),(300,300),(400,600),(600,150)], 5, (255,0,0))
+        self.get_xy()
         while True:
             # 1. 设置刷新帧率
             self.clock.tick(FRAME_PER_SEC)
@@ -36,25 +39,45 @@ class TrackGame(object):
             self.__update_sprites()
             # 5. 更新显示
             pygame.display.update()
+    
+    def env_restart(self):
+        self.car1.LINES_LIST = [(100,620),(100,620)]
+        self.LINES_LIST_NEW = []
+        self.screen.fill((255,255,255))
+        self.car1.k = 1
+        self.car1.positionx = 100
+        self.car1.positiony = 620
 
     def detection_error(self):
-        b = 0
         c = 0
+        d = 0
+        for i in range(1,720):
+            for j in range(1,720):
+                a = tuple(pygame.Surface.get_at(self.screen,(i,j)))
+                if a == (0,255,0,255):
+                    self.path_point.append((i,j))
+
+        for i in self.path_point:
+            if not i in self.LINES_LIST_NEW:
+                self.LINES_LIST_NEW.append(i)
+        for i in self.LINES_LIST_NEW:
+            for j in POINT_COLOUR_xy:
+                if i[0] == j[0]:
+                    c += abs(i[1]-j[1])
+                    d += 1
+
+        print(c)
+        
+    
+    def get_xy(self):
+        b = 0
         for i in range(1,720):
             for j in range(1,720):
                 a = tuple(pygame.Surface.get_at(self.screen,(i,j)))
                 if a == (255,0,0,255):
                     b += 1
                     POINT_COLOUR_xy.append((i,j))
-        for i in LINES_LIST:
-            if not i in LINES_LIST_NEW:
-                LINES_LIST_NEW.append(i)
-        for i in LINES_LIST_NEW:
-            for j in POINT_COLOUR_xy:
-                if i[0] == j[0]:
-                    c += abs(i[1]-j[1])
-        print(c)
-                
+        print(b)
 
     def __event_handler(self):
         for event in pygame.event.get():
@@ -85,6 +108,9 @@ class TrackGame(object):
             self.car1.moderate()
         elif keys_pressed[pygame.K_SPACE]:
             self.car1.bracking()
+        
+        if keys_pressed[pygame.K_r]:
+            self.env_restart()
 
     def __check_collide(self):
         print(self.car1.k)
@@ -93,9 +119,10 @@ class TrackGame(object):
         self.back_group.draw(self.screen)
         pygame.draw.circle(self.screen,CAR_COLOUR,TARGET_POSITION,CAR_SIZE,0)  #最后一个0表示填充，数字代表线宽
         # self.car_group.car_add(self.screen)
-        # self.car_group.car_update()
+        # print(type(self.car_group))
+        self.car_group.car_speedx = 2
         pygame.gfxdraw.bezier(self.screen, [(100,620),(300,300),(400,600),(600,150)], 5, (255,0,0))
-        pygame.draw.lines(self.screen, (0,0,0), 0, LINES_LIST)
+        pygame.draw.lines(self.screen, (0,255,0), 0, self.car1.LINES_LIST)
         self.car1.car_add(self.screen)
         self.car1.car_update()
 
@@ -113,6 +140,6 @@ if __name__ == '__main__':
     pygame.init()
     # 创建游戏对象
     game = TrackGame()
-
+    game.env_restart()
     # 启动游戏
     game.start_game()
